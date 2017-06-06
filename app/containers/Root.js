dea0d6445d35a0f5924470690469994d513311f2
chie_4 <w310930920@gmail.com>
2017-06-06 16:52:02 +0800

    eeeee

---
+++ app/containers/Root.js
@@ -0,0 +1,27 @@
+// @flow
+import React from 'react';
+import { Provider } from 'react-redux';
+import './Root.css';
+// import { ConnectedRouter } from 'react-router-redux';
+/* import Routes from '../routes';
+
+type RootType = {
+  store: {},
+  history: {}
+};
+
+export default function Root({ store, history }: RootType) {
+  return (
+    <Provider store={store}>
+      <ConnectedRouter history={history}>
+        <Routes />
+      </ConnectedRouter>
+    </Provider>
+  );
+}
+*/
+export default function Root() {
+  return (
+    <div>14</div>
+  );
+}
