430ff61cccf5937e8ef07d60ee7a06ee4d750c05
chie_4 <w310930920@gmail.com>
2017-06-18 18:25:50 +0800

    lskdjf

--- app/reducers/index.js
+++ app/reducers/index.js
@@ -1,7 +1,7 @@
 const rootReducer = function rootReducer(state = { show: 'showAll' }, action) {
   switch (action.type) {
     case 'setFilter':
-      return { show: state.show + 1 };
+      return { show: state.show + 2 };
     default:
       return state;
   }
