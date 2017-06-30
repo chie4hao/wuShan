8fb40df84f396e83691d1fe2cebcbc04b7147a4e
chie_4 <w310930920@gmail.com>
2017-06-30 21:13:31 +0800

    lksjdf

--- app/reducers/index.js
+++ app/reducers/index.js
@@ -4,7 +4,7 @@ import { routerReducer } from 'react-router-redux';
 const rootReducer = function rootReducer(state = { show: 'showAll' }, action) {
   switch (action.type) {
     case 'setFilter':
-      return { show: state.show + 2 };
+      return { show: state.show + 1 };
     default:
       return state;
   }
